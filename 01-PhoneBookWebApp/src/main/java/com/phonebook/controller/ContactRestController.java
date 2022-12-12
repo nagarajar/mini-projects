482dcffc19beee4b2cc684b4f4f12374051a437a
nagarajar <nagarajar70@gmail.com>
2022-12-12 09:56:19 +0530

    Added RestController

--- 01-PhoneBookWebApp/src/main/java/com/phonebook/controller/ContactRestController.java
+++ 01-PhoneBookWebApp/src/main/java/com/phonebook/controller/ContactRestController.java
@@ -1,5 +1,63 @@
 package com.phonebook.controller;
 
-public class ContactRestController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.phonebook.entity.Contact;
+import com.phonebook.service.IContactService;
+
+@RestController
+@RequestMapping("/v1/app/contact")
+@CrossOrigin
+public class ContactRestController 
+{
+	@Autowired
+	private IContactService service;
+	
+	@PostMapping("/create")
+	public ResponseEntity<String> createContact(@RequestBody Contact contact){
+		String msg = service.saveContact(contact);
+		return new ResponseEntity<String>(msg, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/all")
+	ResponseEntity<List<Contact>> getAllContacts(){
+		List<Contact> contacts = service.getAllContacts();
+		return new ResponseEntity<List<Contact>>(contacts,HttpStatus.OK);
+	}
+	
+	@GetMapping("/find/{id}")
+	ResponseEntity<?> getOneContact(@PathVariable Integer id){
+		Contact contact = service.getContactById(id);
+		if(contact != null) {			
+			return new ResponseEntity<Contact>(contact, HttpStatus.OK);
+		}
+		String msg = "Contact Not Found";
+		return new ResponseEntity<String>(msg, HttpStatus.NOT_FOUND);
+	}
+	
+	@PutMapping("/modify")
+	ResponseEntity<String> modifyContact(@RequestBody Contact contact){
+		String msg = service.updateContact(contact);
+		return new ResponseEntity<String>(msg, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/remove/{id}")
+	ResponseEntity<String> deleteContact(@PathVariable Integer id){
+		String msg = service.deleteContactById(id);
+		return new ResponseEntity<String>(msg, HttpStatus.OK);
+	}
+	
 }
--- 01-PhoneBookWebApp/src/main/java/com/phonebook/entity/Contact.java
+++ 01-PhoneBookWebApp/src/main/java/com/phonebook/entity/Contact.java
@@ -1,20 +1,27 @@
 package com.phonebook.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "CONTACT_DETAILS")
 public class Contact 
 {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "CONTACT_ID")
 	private Integer contactId;
+	@Column(name = "CONTACT_NAME")
 	private String contactName;
+	@Column(name = "CONTACT_EMAIL")
 	private String contactEmail;
+	@Column(name = "CONTACT_NUMBER")
 	private Long contactNum;
 }
